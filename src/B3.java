9bc4cbf0c8553be04d21f97ab3598b5d7b2f6a16
Humeyra Uysal <humeyrasahinuysal@gmail.com>
2022-12-22 12:13:08 +0100

    branch v3

---
+++ src/B3.java
@@ -0,0 +1,12 @@
+public class B3 {
+    public static void main(String[] args) {
+        /*
+        Projemizde onemli bir degisiklik yapmak istiyoruz.
+        Bu degisiklige 10 gun calisacagiz
+        degisikligi yapmaya devam ederken orginal projemizi riskke atmamak ev
+        eger son kullanici kullaniyorsa olusabilecek sorunlari engellemek icn
+        orginal proje uzerinde git
+
+         */
+    }
+}
