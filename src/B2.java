983ae6aff046809b2d64fb41f0d6bbf2177e09d8
Humeyra Uysal <humeyrasahinuysal@gmail.com>
2022-12-22 11:53:47 +0100

    son hali

---
+++ src/B2.java
@@ -0,0 +1,22 @@
+public class B2 {
+    public static void main(String[] args) {
+
+        /*
+        temelde git ve github icin uc asamadan bahsedebiliriz.
+        1- projemizi/klasorumuz ile git i tanistirma
+        git init
+        2- projemiz/klasorumuz icin versiyon olusturuma
+        git add .
+        git commit -m "versiyon adi"
+        3- github ile entegrasyon ve senkronizasyon
+        -ilk defa entegre etmek icin once github da yeni bir repo olusturup
+        oradaki son 3 adimi l-kopyalayip teminel/CMD ye yapistirip
+        enter a basacagiz.
+        - ilk entegrasyondan sonra her degisiklik yaptigimizda
+        -git add.
+        -git commit -m "versiyon adi"
+        -git push
+
+         */
+    }
+}
